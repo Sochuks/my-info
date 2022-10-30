af0fc77ee3d5f8b4778d41b0a6fb28ca7139c94a
Sochuks <moseschukwumakajang@gmail.com>
2022-10-30 05:43:46 +0100

    - Created a Details model,stores:   - slackUsername   - backend   - age   - bio - Made migrations to db

--- myapi/models.py
+++ myapi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Details(models.Model):
+    slackUsername = models.CharField(max_length=20)
+    backend = models.BooleanField()
+    age = models.IntegerField()
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.slackUsername
